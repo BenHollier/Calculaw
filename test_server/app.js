a8ae270bd20bb466e3bd9bc17f1c15f5e2649569
Red Weka <ben4261@hotmail.com>
2021-01-20 11:22:54 +1300

    Install helmet and compression and add to app.js

--- test_server/app.js
+++ test_server/app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const compression = require('compression');
+const helmet = require('helmet')
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
@@ -19,6 +22,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(helmet())
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
